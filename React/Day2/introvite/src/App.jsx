eb47dcaa40d3967a548b6fa1c0e80da66b6ee406
bhaarath123 <bhaarath9080@gmail.com>
2026-09-17 09:30:27 +0530

    Day 2 complete

---
+++ React/Day2/introvite/src/App.jsx
@@ -0,0 +1,29 @@
+const App =() =>{
+  return(<>
+  <div>
+    This is my React application 
+  </div>
+<input/>
+<Button/>  
+  
+  </>)
+}
+
+export default App
+  
+
+const Input =() =>{
+  return(<>
+  
+  <input type ="text"/>
+  
+  </>)
+}
+
+const Button =() =>{
+  return(<>
+  
+  <button>click now</button>
+  
+  </>)
+}
